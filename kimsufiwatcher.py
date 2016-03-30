bc956d3eed3ed9f7e4c1fb8c8b73a588c2a73a98
Luis González Fernández <luisgf@luisgf.es>
2016-03-30 19:19:38 +0200

    Update models to 2016

--- kimsufiwatcher.py
+++ kimsufiwatcher.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 """
     KimSufi watcher, By Luis González Fernández
 
     luisgf at luisgf . es, 2015
-    
+
     Catch a kimsufi server is hard... with this tool i hope
     that your chances to catch one grow a bit.
 
@@ -16,23 +16,24 @@ import sys
 from bs4 import BeautifulSoup
 
 # Kimsufi models
-kimsufis = {'KS-6':'150sk60','KS-5':'150sk50','KS-4':'150sk40','KS-3':'150sk30','KS-2 SSD':'150sk22','KS-2':'150sk20','KS-1':'150sk10'}
+kimsufis = {'KS-5':'160sk5','KS-4A':'160sk4','KS-3B':'160sk31','KS-3A':'160sk3','KS-2A':'160sk2','KS-1':'160sk1'}
 
 # Models to watch
-watch = ['KS-4'] 
+watch = ['KS-3A']
 
-def check_kimsufi(data, model):  
+def check_kimsufi(data, model):
     date = time.strftime('%d/%m/%Y %H:%M:%S')
     if model not in kimsufis:
         print('KimSufi %s model incorrect' % model)
         sys.exit(-1)
-    
-    soup = BeautifulSoup(data)    
+
+    soup = BeautifulSoup(data, 'html.parser')
     table = soup.find('table', {'class': 'full homepage-table'})
     tr = table.find('tr', {'data-ref': kimsufis[model]})
     td = tr.find('td', {'class': 'show-on-ref-unavailable elapsed-time-since-last-delivery'})
-    
-    if 'En cours de réapprovisionnement' in td.text:
+    qty = td.find('div', {'class':'QTYBox'})
+
+    if qty:
         print('%s Kimsufi %s Not Available' % (date, model))
         return True
     else:
@@ -43,10 +44,10 @@ def check_kimsufi(data, model):
 if __name__ == '__main__':
     rc = True
     while rc:
-        r = requests.get('http://www.kimsufi.com/fr/index.xml')
+        r = requests.get('http://www.kimsufi.com/fr/serveurs.xml')
 
         for model in watch:
             rc = check_kimsufi(r.text, model)
-            
+
         time.sleep(60)
-        
+
